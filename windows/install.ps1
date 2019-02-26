7949c1586f58604f91855b8db760d26082432575
ayushio <multiverse2011@gmail.com>
2019-02-26 12:22:34 +0900

    Added IntelliJ install script

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,18 +1,13 @@
 ## robop install tool ##
-
-Write-Host "Start install sctipt..."
-
-$ScriptDir = Split-Path $MyInvocation.MyCommand.Path
-
 Function InstallJdk {
 
     Write-Host "Installing OracleJDK..."
-    cmd /c "start ${ScriptDir}\jdk-8u201-windows-x64.exe /s /lang=1041 INSTALLDIR=C:\java\jdk1.8.201\ /L ${ScriptDir}\JDKsetup.log" 
+    Start-Process -FilePath ${ScriptDir}\jdk-8u201-windows-x64.exe -ArgumentList "/s" -Wait
     Write-Host "Done."
     
     Write-Host "Setting system PATH..."
     $UserPath = [Environment]::GetEnvironmentVariable('PATH', 'User')
-    $UserPath += ';' +　"C:\java\jdk1.8.201\bin\"
+    $UserPath += ';' + "C:\java\jdk1.8.201\bin\"
     [Environment]::SetEnvironmentVariable('PATH', $UserPath, 'User')
     Write-Host "Done."
     
@@ -23,9 +18,42 @@ Function InstallJdk {
     Write-Host "Done."
     Write-Host ("Current PATH:" + $UserPath)
     Write-Host ("Current JAVA_HOME:" + $JavaHome)
+
+    return
 }
 
 Function InstallIntellij {
-    
+    Write-Host "Installing IntelliJ IDEA..."
+    Write-Host "Unzip IntelliJ IDEA..."
+    Expand-Archive $ScriptDir\ideaIC-2018.3.4.win.zip $ScriptDir
+    Write-Host "Done."
+    Write-Host "Setting user configuration..."
+
+    Write-Host "Done."
+
+    return
 }
+
+Function SetSpicapc{
+    Write-Host "Unzip Spica-pc"
+    Expand-Archive $ScriptDir\ideaIC-2018.3.4.win.zip $ScriptDir
+    Write-Host "Done."
+}
+
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+            [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Start install sctipt..."
+    $ScriptDir = Split-Path $MyInvocation.MyCommand.Path
+    InstallJdk
+    InstallIntellij
+    SetSpicapc
+    Write-Host "Finish install script!!"
+}
+
+else {
+    Write-Warning "You are not Administrator!!"
+    Write-Warning "Failed."
+    exit 1
+}
+
 
